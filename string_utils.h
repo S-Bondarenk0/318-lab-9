96ffe976c903219fb09eba1b3017d5bf5caa969a
S-Bondarenk0 <bondarenkoseryj4@gmail.com>
2025-05-28 11:55:00 +0300

    Add files via upload

---
+++ string_utils.h
@@ -0,0 +1,12 @@
+#ifndef STRING_UTILS_H
+#define STRING_UTILS_H
+
+#include <string>
+
+// Завдання 1: реалізація методу string::append(n, c)
+void task1();
+
+// Завдання 2: обробка рядків, подвоєння символів
+void task2();
+
+#endif // STRING_UTILS_H
